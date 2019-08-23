6925f866674dfba883ebaa6629a7e028c2f14426
root <pardeep@orangemantra.in>
2019-08-23 15:00:11 +0530

    added attr

--- src/components/form-child/Number.js
+++ src/components/form-child/Number.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Number extends Component{
     render(){
         return(
-            <input type="number" name="{this.props.name}"/>
+            <input type="number" className="test" name="{this.props.name}"/>
         )
     }
 }
